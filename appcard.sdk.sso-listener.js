9cf3801c7981a5f7011ae13f9c8b80af94df2e6c
Moti Gust <seadrum@gmail.com>
2025-07-07 14:44:54 +0300

    fix

--- appcard.sdk.sso-listener.js
+++ appcard.sdk.sso-listener.js
@@ -10,10 +10,7 @@ class AppCardSSOListener extends HTMLElement {
         const iframeStyle = modeProperty === 'debug' ? 'display:none; width:0; height:0; border:0;' : 'display:block; width:500px; height:500px; border:1px solid black;';
 
         this/*.shadow*/.innerHTML = `
-            <script>
-            </script>
-
-            <button id="login-button">test</button>
+            <button id="login-button">Test connect event</button>
             <iframe
                 id="silent-auth-frame"
                 src="https://auth.test.appcard.com/authorize?client_id=m2m-client-test-4f155389-ec66-4ccc-a739-bae6c4fc2d2a&response_type=code&scope=openid&state=274322b9-ea1d-459e-8c32-b4df9f513013&redirect_uri=https://seadrum.github.io/site/silent-callback.html&nonce=b2b0a38d-2e05-4944-bb47-ecb6c511eb5f"
@@ -28,29 +25,32 @@ class AppCardSSOListener extends HTMLElement {
 
     connectedCallback() {
         console.log('AppCardSSOListener connected');
+        const onAuthCodeReceivedHandler = this.getAttribute('onAuthCodeReceived');
+
         this/*.shadow*/.querySelector('button').addEventListener('click', () => {
-//        const code = 'abc123';
+            const code = 'abc123';
+            if (onAuthCodeReceivedHandler && typeof window[onAuthCodeReceivedHandler] === 'function') {
+                window[onAuthCodeReceivedHandler](code); // Call the function by name from global scope
+            }
+        }, false);
 
 
-        console.log('Setting up iframe listener');
-        var code = null;
         window.addEventListener("message", function (event) {
             console.log("Received message from iframe:", event.data);
             console.log("Received message type from iframe:", event.data.type);
+            var code = null;
+
             if (event.data.type === "OIDC_CODE") {
 
-
-                        const onAuthCodeReceivedHandler = this.getAttribute('onAuthCodeReceived');
 
                 if (onAuthCodeReceivedHandler && typeof window[onAuthCodeReceivedHandler] === 'function') {
                     window[onAuthCodeReceivedHandler](code); // Call the function by name from global scope
                 }
+            }
                     /*
                 code = event.data.code;
                 document.getElementById("status").innerText = "Received code: " + code;
                 */
-            }
-        }, false);
 
 
 
